a6e5de677a2b734951f84892ad6726d50a91600f
vavsn <41681585+vavsn@users.noreply.github.com>
2021-09-19 17:54:54 +0700

    Lesson 5. Homework. Performance counters.

---
+++ MetricsAgent/DAL/Migrations/Migrations.cs
@@ -0,0 +1,43 @@
+﻿using FluentMigrator;
+using System;
+
+namespace MetricsAgent.DAL.Migrations
+{
+
+    [Migration(4)]
+    public class FirstMigration : Migration
+    {
+        private string[] arrDB = new string[] { "cpumetrics", "dotnetmetrics", "hddmetrics", "networkmetrics", "objmetrics", "rammetrics" };
+
+        public override void Up()
+        {
+            // удалим имеющиеся таблицы
+            Down();
+            for (int i = 0; i < arrDB.Length; i++)
+            {
+                if (!Schema.Table(arrDB[i]).Exists())
+                {
+                    Create.Table(arrDB[i])
+                        .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                        .WithColumn("Value").AsInt32()
+                        .WithColumn("Time").AsInt64();
+                    for (int j = 1; j <= 6; j++)
+                    {
+                        Insert.IntoTable(arrDB[i])
+                          .Row(new { Id = j, Value = 100 * j, Time = j });
+                    }
+                }
+            }
+        }
+
+
+        public override void Down()
+        {
+            for (int i = 0; i < arrDB.Length; i++)
+            {
+                if (Schema.Table(arrDB[i]).Exists())
+                    Delete.Table(arrDB[i]);
+            }
+        }
+    }
+}
---
+++ MetricsAgent/Startup.cs
@@ -0,0 +1,103 @@
+
+using AutoMapper;
+using FluentMigrator.Runner;
+using MetricsAgent.DAL;
+using MetricsAgent.Jobs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+
+namespace MetricsAgent
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private const string ConnectionString = @"Data Source=metrics.db; Version=3;";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<ICpuMetricsRepository, CpuMetricsRepository>();
+            services.AddSingleton<IDotNetMetricsRepository, DotNetMetricsRepository>();
+            services.AddSingleton<IHddMetricsRepository, HddMetricsRepository>();
+            services.AddSingleton<INetworkMetricsRepository, NetworkMetricsRepository>();
+            services.AddSingleton<IObjMetricsRepository, ObjMetricsRepository>();
+            services.AddSingleton<IRamMetricsRepository, RamMetricsRepository>();
+            var mapperConfiguration = new MapperConfiguration(mp => mp.AddProfile(new MapperProfile()));
+            var mapper = mapperConfiguration.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // добавляем поддержку SQLite 
+                    .AddSQLite()
+                    // устанавливаем строку подключения
+                    .WithGlobalConnectionString(ConnectionString)
+                    // подсказываем где искать классы с миграциями
+                    .ScanIn(typeof(Startup).Assembly).For.Migrations()
+                ).AddLogging(lb => lb
+                    .AddFluentMigratorConsole());
+
+            // сервисы запуска заданий по-времени
+            // Добавляем сервисы
+            services.AddSingleton<IJobFactory, SingletonJobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+            services.AddSingleton<CpuMetricJob>();
+            services.AddSingleton<RamMetricJob>();
+            services.AddSingleton<DotNetMetricJob>();
+            services.AddSingleton<HddMetricJob>();
+            services.AddSingleton<NetworkMetricJob>();
+            // добавляем задачи
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(CpuMetricJob),
+                cronExpression: "0/5 * * * * ?")); // запускать каждые 5 секунд
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(RamMetricJob),
+                cronExpression: "0/5 * * * * ?"));
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(DotNetMetricJob),
+                cronExpression: "0/5 * * * * ?"));
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(HddMetricJob),
+                cronExpression: "0/5 * * * * ?"));
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(NetworkMetricJob),
+                cronExpression: "0/5 * * * * ?"));
+
+            services.AddHostedService<QuartzHostedService>();
+
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMigrationRunner migrationRunner)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            // запускаем миграции
+            migrationRunner.MigrateUp();
+        }
+    }
+}
